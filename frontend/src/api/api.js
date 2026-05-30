775f0af057ae46486adb0746b9ba0a277ebf1fbc
HuynhNhu <0306231322@caothang.edu.vn>
2026-05-30 13:01:40 +0700

    feat(user): view detail actitivity type

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authConfig from '../config/authConfig';
 
-const API_BASE_URL = authConfig.apiBaseUrl;
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
 const TOKEN_KEYS = authConfig.tokenKeys;
 const USER_KEY = authConfig.userKey;
 
